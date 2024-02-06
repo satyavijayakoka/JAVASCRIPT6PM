320c822ac0797326808185bce5091e48f307fff2
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-06 10:46:14 -0500

    firstcommit

---
+++ script.js
@@ -0,0 +1,86 @@
+// program 1
+// let a=10
+// console.log(a)
+// a=12
+// console.log(a)
+
+// const h=11
+// console.log(h)
+//h=13
+//console.log(h)//Assignment to constant variable.
+
+// program 2
+// arithmetic operations
+// let q=12
+// let r=6
+// console.log(q+r)
+// console.log(q-r)
+// console.log(q*r)
+// console.log(q/r)
+// console.log(q%r)
+
+// let s=6
+// let t=2
+// console.log(s+t)
+// console.log(s-t)
+// console.log(s*t)
+// console.log(s/t)
+// console.log(s%t)
+
+// program 3
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(8,4)
+// Calculator(12,4)
+// Calculator(32,4)
+
+// program 4
+// function without parameter and without return type
+function addA(){
+    console.log(2+2)
+}
+addA()
+addA()
+addA()
+
+// function with parameter and without return type
+function addB(x,y){
+    console.log(x+y)
+}
+addB(6,3)
+addB(15,3)
+addB(25,3)
+
+// function with parameter and with return type
+function addC(x,y){
+    return x+y
+}
+let q1 =addC(21,7)
+console.log(q1)
+console.log(q1+q1)
+console.log(q1*q1)
+console.log(q1*0.10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

4d448df3b7bbacf5fcd82f2ea59fe6cb68b9affd
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-07 12:53:37 -0500

    day2

---
+++ script2.js
@@ -0,0 +1,97 @@
+// program 1
+// types 
+
+// Introvert   // extrovert
+
+//calm           // loud
+//less outing    // more outing 
+//less social    // more social
+
+
+// Human --- type ----->
+// Properties - color , weight , height 
+// Methods  - walk() , talk()
+
+// Vehicle --- type ----> 
+// Properties - color , type ,regNo , avg
+// Methods - start() , stop()
+
+// Bank acc 
+// Propeties --- accNo , accType , bal , IFSC code
+// Method - withdrawl() , deposit()
+
+let a=10
+console.log(a)
+console.log(typeof a)
+
+let y=true
+console.log(y)
+console.log(typeof y)
+
+let c="satya"
+console.log(c)
+console.log(typeof c)
+
+// values are same and type are different
+console.log(typeof 10)
+console.log(typeof '10')
+
+// comparison operator
+// program 2
+console.log(5 === '5') // false
+console.log(5 !== '5') // true
+console.log(5 === 5) // true
+console.log(7 !== 7) // false
+console.log(7 == '7') // true
+console.log(7 != '7') // false
+console.log(7 != 7) // false
+console.log(7 > 3) // true
+console.log(7 < 3) // false
+console.log(7 >= 3) // true
+console.log(7 <= 3) // false
+console.log(7 <= 7) // true
+console.log(7 >= 7) // true
+
+// program 3
+// logical operator
+//&&  - and
+// true    &&     true   =====> true 
+// false   &&     true   =====> false
+// true    &&     false  =====> false
+// false   &&     false  =====> false
+
+console.log(8 == 9 && 9 == 9)
+console.log(8 == 8 && 9 == 9)
+console.log(8 == 8 && 9 != 9)
+console.log(8 != 8 && 9 != 9)
+
+// || - or
+// true    ||     true   =====> true 
+// false   ||     true   =====> true
+// true    ||     false  =====> true
+// false   ||     false  =====> false
+
+console.log(8 == 9 || 9 == 9)
+console.log(8 == 8 || 9 == 9)
+console.log(8 == 8 || 9 != 9)
+console.log(8 != 8 || 9 != 9)
+
+// ! - not
+console.log(!(8 == 8))
+console.log(!(9 != 9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

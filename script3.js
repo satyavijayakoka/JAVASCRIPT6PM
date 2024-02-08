a058f083fc676e6cdf9fd08a661a7abf07fbdb9a
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-08 11:58:17 -0500

    day3

---
+++ script3.js
@@ -0,0 +1,86 @@
+// conditional statements
+let numT =15
+// if(numT>0 && numT<=5){
+//     console.log("10% discount")
+// }
+// if(numT>5 && numT<=10){
+//     console.log("20% discount")
+// }
+// if(numT>10){
+//     console.log("30% discount")
+// }
+
+// program 2
+// if(numT>0 && numT<=5){
+//     console.log("10% discount")
+// }
+// else if(numT>5 && numT<=10){
+//     console.log("20% discount")
+// }
+// else if(numT>10){
+//     console.log("30% discount")
+// }
+// else{
+//     console.log("incorrect value")
+// }
+
+// program 3
+
+let marks =92
+// if(marks >= 90){
+//     console.log("Grade A")
+// }
+// if(marks >= 75){
+//     console.log("Grade B")
+// }
+// if(marks >= 65){
+//     console.log("Grade C")
+// }
+// program 4
+if(marks >= 90){
+        console.log("Grade A")
+    }
+else if(marks >= 75){
+        console.log("Grade B")
+    }
+ else  if(marks >= 65){
+        console.log("Grade C")
+    }
+else{
+    console.log("please try again..")
+}
+
+// program 5
+
+let a=10
+let b=5
+if(a>b){
+    console.log("a is greater")
+}
+else{
+    console.log("b is greater")
+}
+
+// program 6
+let x1=10
+let x2=20
+let x3=30
+if(x1>x2 && x1>x3){
+    console.log("x1 is greater")
+}
+else if(x2>x1 && x2>x3){
+    console.log("x2 is greater")
+}
+else{
+    console.log("x3 is greater")
+}
+
+// ternary operator
+let q=5
+let r=3
+q>r ? console.log("q is greater"): console.log("r is greater")
+
+let age=15
+let q1 = age>=18 ? "can drive": "can not drive"
+console.log(q1)
+

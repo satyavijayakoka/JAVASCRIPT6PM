0e593d393745833762b4675703eeb2100f58d44d
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-13 11:38:43 -0500

    day5

---
+++ script5.js
@@ -0,0 +1,52 @@
+// loops - for / while 
+// for(inilialization;condition ; increment){
+//     // statements
+// }
+
+// program 1
+for(let i=1; i<=3; i++){
+    console.log(i)
+}
+
+// program 2
+for(let i=1; i<=5; i++){
+    console.log(i)
+}
+
+// program 3
+for(let i=5; i>=1; i--){
+    console.log(i)
+}
+
+// program 4
+for(let i=1; i<=10; i++){
+    console.log(i*2)
+}
+
+for(let i=2; i<=20; i=i+2){
+    console.log(i)
+}
+
+// program 5
+// print table of 5 in reverse
+
+for(let i=50; i>=5; i=i-5){
+    console.log(i)
+}
+
+// program 6
+for(let i=6; i<=60; i=i+6){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+

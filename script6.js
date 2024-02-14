5795d31d3408467daacd35076aa559974c571952
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-14 13:08:31 -0500

    day6

---
+++ script6.js
@@ -0,0 +1,88 @@
+// for loop 
+
+// for(intialization ; conditionCheck ; increment/decrement){
+//     // statements 
+// }
+
+// program 1
+// for(let i=1; i<=3; i++){
+//     console.log(i)
+
+// }
+
+// for(let i=3; i>=1; i--){
+//     console.log(i)
+
+// }
+
+// for(let i=2; i<=20; i=i+2){
+//     console.log(i)
+// }
+
+// program 2 loop with break
+// for(let i=1; i<=5; i++){
+//     if (i==3){
+//         break
+//     }
+//     console.log(i)
+// }
+// for(let i=1; i<=5; i++){
+//     console.log(i)
+//     if(i==3){
+//         break
+//     }
+// }
+
+// // program 3
+// for(let i=5; i>=1; i--){
+//     if(i==3){
+//         break
+//     }
+//     console.log(i)
+// }
+
+// // program 4
+// for(let i=2; i<=20; i=i+2){
+//     if (i==10){
+//         break
+//     }
+//     console.log(i)
+// }
+
+// program 5 continue
+for(let i=1; i<=5; i++){
+    if(i==3){
+        continue
+    }
+    console.log(i)
+}
+
+// program 6
+for(let i=5; i>=1; i--){
+    if(i==2){
+        continue
+    }
+    console.log(i)
+}
+
+// program 7
+for(let i=50; i>=5; i=i-5){
+    if(i==35){
+        continue
+    }
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
